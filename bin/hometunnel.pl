b40c15163380b2c131d69cc97998b31106515b3e
Conor Schaefer <conor.schaefer@gmail.com>
2012-05-27 13:30:19 -0400

    Better failure feedback
    Implemented timeout counter (default 30 seconds) in case connection cannot be established. This value is user-customizable (though not yet from command line) and the `die` feedback references this value, as well.

--- bin/hometunnel.pl
+++ bin/hometunnel.pl
@@ -6,7 +6,8 @@ use strict;
 use warnings;
 
 my $host = "t"; #host computer to connect to via SSH (this will reference SSH conf file, so it's OK to use aliases);
-my $port = "2002"; #local port through which the proxy connection will be accessible;
+my $port = 2002; #local port through which the proxy connection will be accessible;
+my $timeout_duration = 30; #after this many seconds, ssh will give up and return user to shell;
 
 #my $ssh_pid = `ps ax | grep "ssh -D 2002" | head -n 1 | awk '{print $1}'`;#Get PID of previously running tunnel, if it exists;
 my $ssh_pid = `ps ax | grep "ssh -D 2002" | head -n 1`; #Grab last first line of ps output, so other tunnels can be killed;
@@ -19,6 +20,8 @@ if ($ssh_pid) { #Check whether a PID for a pre-existing SSH tunnel was found
 #    `kill $ssh_pid`; #If so, kill it;
 }
 print "Attempting to open ssh tunnel now...\n"; #Give the user a little feedback;
-`ssh -D 2002 -f -q -N -C $host` or die "Unable to open SSH tunnel; check connection to internet."; #Perform the actual connection;
+`ssh -D 2002 -f -q -N -o ConnectTimeout=$timeout_duration -C $host` #Perform the actual connection;
+    or die "    After $timeout_duration seconds, no connection could be established.
+    Please check connection to internet."; #Report failure if unable to connect;
 #system ("ssh -D $port -f -q -N -C $host"); #Perform the actual connection;
 print "SSH tunnel initiated. Enjoy!\n"; #This 
