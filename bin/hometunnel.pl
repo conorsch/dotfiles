596d4bf74916653d9f4552a43edc6adfd80b0409
Conor Schaefer <conor.schaefer@gmail.com>
2012-05-28 14:43:27 -0400

    Better handling of pre-existing processes
    Added logic to remove already-running SSH tunnels on same port. Previously, only one pre-existing process could be handled, and now it scales well. Hopefully such a corner case will only occur during debugging, but it's still nice to have the code in place.
    
    Currently the SSH tunnel command doesn't seem to close... the kills work fine, but then the SSH command never returns. Perhaps I should be using system() instead of backticks?

--- bin/hometunnel.pl
+++ bin/hometunnel.pl
@@ -4,24 +4,32 @@
 #web browser settings to use the SOCKS proxy.
 use strict;
 use warnings;
+use feature qw/switch say/;
 
 my $host = "t"; #host computer to connect to via SSH (this will reference SSH conf file, so it's OK to use aliases);
 my $port = 2002; #local port through which the proxy connection will be accessible;
-my $timeout_duration = 30; #after this many seconds, ssh will give up and return user to shell;
+my $timeout_duration = 5; #after this many seconds, ssh will give up and return user to shell;
 
 #my $ssh_pid = `ps ax | grep "ssh -D 2002" | head -n 1 | awk '{print $1}'`;#Get PID of previously running tunnel, if it exists;
-my $ssh_pid = `ps ax | grep "ssh -D 2002" | head -n 1`; #Grab last first line of ps output, so other tunnels can be killed;
-chomp $ssh_pid; #Remove that pesky trailing newline;
-undef $ssh_pid if ($ssh_pid =~ m/grep/); #We don't want the grep process, which will show up on the ps output;
-if ($ssh_pid) { #Check whether a PID for a pre-existing SSH tunnel was found
-    $ssh_pid =~ s/^ (\d+)(.*)$/$1/; #Grab just the PID from the output (skip first space, get numbers, forget rest;
-    print "Pre-existing SSH tunnel found as process $ssh_pid, killing it.\n";
-    system ("kill $ssh_pid"); #If so, kill it;
-#    `kill $ssh_pid`; #If so, kill it;
+my @preexisting_tunnels = `ps ax | grep "ssh -D 2002"`; #Grab last first line of ps output, so other tunnels can be killed;
+foreach my $preexisting_tunnel (@preexisting_tunnels) {
+    chomp $preexisting_tunnel; #remove that pesky trailing newline;
+    next if ($preexisting_tunnel =~ m/grep/); #We don't want the grep process, which will show up on the ps output;
+    $preexisting_tunnel =~ s/^(\d+)(.*)$/$1/; #Grab just the PID from the output (skip first space, get numbers, forget rest;
+    `kill $preexisting_tunnel`; #If so, kill it;
+    say "Killing preexisting tunnel with PID $preexisting_tunnel...";
 }
-print "Attempting to open ssh tunnel now...\n"; #Give the user a little feedback;
+
+#chomp $ssh_pid; #Remove that pesky trailing newline;
+#undef $ssh_pid if ($ssh_pid =~ m/grep/); #We don't want the grep process, which will show up on the ps output;
+#if ($ssh_pid) { #Check whether a PID for a pre-existing SSH tunnel was found
+#    $ssh_pid =~ s/^ (\d+)(.*)$/$1/; #Grab just the PID from the output (skip first space, get numbers, forget rest;
+#    print "Pre-existing SSH tunnel found as process $ssh_pid, killing it.\n";
+#    system ("kill $ssh_pid"); #If so, kill it;
+#}
+say "Attempting to open ssh tunnel now..."; #Give the user a little feedback;
 `ssh -D 2002 -f -q -N -o ConnectTimeout=$timeout_duration -C $host` #Perform the actual connection;
     or die "    After $timeout_duration seconds, no connection could be established.
     Please check connection to internet."; #Report failure if unable to connect;
 #system ("ssh -D $port -f -q -N -C $host"); #Perform the actual connection;
-print "SSH tunnel initiated. Enjoy!\n"; #This 
+say "SSH tunnel initiated. Enjoy!"; #This 
