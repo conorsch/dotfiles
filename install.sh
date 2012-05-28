2a688c7004a5ad7f32ac4c67ac07d05f872c4c2e
Conor Schaefer <conor.schaefer@gmail.com>
2012-05-28 13:20:00 -0400

    More complete commenting; almost have whole script documented.

--- install.sh
+++ install.sh
@@ -9,27 +9,27 @@ for name in *; do #Look at all files in current directory (*)
     if [ ! -L "$target" ]; then #and if target file is not a symlink
       cutline=`grep -n -m1 "$cutstring" "$target" | sed "s/:.*//"` #search target file for "cutstring" (is target this script?)
       if [ -n "$cutline" ]; then #if cutline is NOT empty
-	cutline=$((cutline-1))
-        echo "Updating $target"
+	    cutline=$((cutline-1))
+        echo "Updating $target" #Provide feedback 
         head -n $cutline "$target" > update_tmp
         startline=`sed '1!G;h;$!d' "$name" | grep -n -m1 "$cutstring" | sed "s/:.*//"`
         if [ -n "$startline" ]; then
           tail -n $startline "$name" >> update_tmp
         else
-          cat "$name" >> update_tmp
+          cat "$name" >> update_tmp #prepare temporary file with contents of target file
         fi
-        mv update_tmp "$target"
-      else
-        echo "WARNING: $target exists but is not a symlink."
+        mv update_tmp "$target" #move temporary file to ~/, renaming it appropriately.
+      else #if cutline string IS empty (see declaration above) 
+        echo "WARNING: $target exists but is not a symlink." #state that clobbering would happen, stop;
       fi
     fi
   else
-    if [ "$name" != 'install.sh' ]; then
-      echo "Creating $target"
+    if [ "$name" != 'install.sh' ]; then #if target script is NOT this script ("install.sh")
+      echo "Creating $target" #provide feedback to user
       if [ -n "$(grep "$cutstring" "$name")" ]; then
-        cp "$PWD/$name" "$target"
+        cp "$PWD/$name" "$target" #copy target script to ~/ (see 'target' declaration above)
       else
-        ln -s "$PWD/$name" "$target"
+        ln -s "$PWD/$name" "$target" #create symlink for target script instead of copying it.
       fi
     fi
   fi
