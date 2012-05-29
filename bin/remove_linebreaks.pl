7d128d769ac5509b89deba1b6192db9c275b52a8
Conor Schaefer <conor.schaefer@gmail.com>
2012-05-29 12:37:57 -0400

    STDIN support
    Converted script to listen on STDIN and print out results. This makes it very pipe-friendly so it can be used in place of sed for quick formatting operations.

--- bin/remove_linebreaks.pl
+++ bin/remove_linebreaks.pl
@@ -1,17 +1,18 @@
 #!/usr/bin/perl 
-#Script to process text and remove linebreaks
+#Script to cleanup plain text by removing hard line breaks,
+#ensuring single spaces between sentences, 
+#and replacing double hypens with true em dashes (—).
+#Recommended usage is with filehandle redirects, e.g.:
+
 use strict;
 use warnings;
 use feature qw/say switch/;
 
-my $input_text = $ARGV[0]; #unpack arguments
-open TEXTIN, '<', $input_text; #create filehandle for manipulating this text
-
-while (<TEXTIN>) { #look at file
-    s/\n/ /; #remove all hard line breaks
+while (<STDIN>) { #read from standard input (pipe-friendly tool)
+    s/\n/ /; #remove all hard line breaks;
     s/\s+/ /g; #remove multiple instances of whitespace, such as double-spacing between sentences;
     s/-{2}/—/g; #replace ugly double hyphens with a real em dash;
+    s/\\"/'/g; #remove escaped quotes, because sadly sometimes that happens;
     printf "$_"; #print line on screen as it's processed;
 }
-close TEXTIN; #put away the file after processing;
 say ""; #add final line break, so prompt appears cleanly;
