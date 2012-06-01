bccc8a0460bd92c3e2659205d8fd0b3a8e74d9eb
Conor Schaefer <conor.schaefer@gmail.com>
2012-06-01 11:16:17 -0400

    Better paragraph support
    Added functionality to read from STDIN to a string, then break on paragraph boundary. Also implemented title formatting, which allows for a title to be extracted and displayed in bold text, but this feature currently has no failsafe. (It should be optional, not mandatory.)

--- bin/remove_linebreaks.pl
+++ bin/remove_linebreaks.pl
@@ -8,12 +8,27 @@
 use strict;
 use warnings;
 use feature 'say'; 
+use Term::ANSIColor; #Allows for bolding of title output;
 
-while (<STDIN>) { #read from standard input (pipe-friendly tool)
-    s/\n/ /; #remove all hard line breaks;
-    s/\s+/ /g; #remove multiple instances of whitespace, such as double-spacing between sentences;
-    s/-{2}/—/g; #replace ugly double hyphens with a real em dash;
-    s/\\"/'/g; #remove escaped quotes, because sadly sometimes that happens;
-    printf "$_"; #print line on screen as it's processed;
-}
-say ""; #add final line break, so prompt appears cleanly;
+my $title = $ARGV[0]; #get title if exists from caller, allowing separate formatting;
+my @paragraphs; #initialize array to store whole paragraphs (with no line breaks);
+
+local $/ = ''; #This allows for reading into a string as a paragraph;
+
+my $raw_text = <STDIN>; #get input data to modify;
+
+#####BEGIN text-processing;
+$raw_text =~ s/$title//; #first match of title should be removed;
+$raw_text =~ s/\n+/ /gs; #remove all hard line breaks;
+$raw_text =~ s/[^\S\n]+/ /gs; #remove multiple instances of whitespace, such as double-spacing between sentences;
+$raw_text =~ s/-{2}/—/g; #replace ugly double hyphens with a real em dash;
+$raw_text =~ s/\\"/'/g; #remove escaped quotes, because sadly sometimes that happens;
+my $processed_text = $raw_text;
+chomp $processed_text; #remove excessive newlines (doesn't appear to remove final \n);
+#####END text-processing;
+
+####Print results
+print color 'bold'; #Make next text be printed bold;
+say $title; #say title, which will appear bold;
+print color 'reset'; #make next text be printed normally, not bold;
+say "$processed_text"; #say rest of text, which will appear normal, not bold;
