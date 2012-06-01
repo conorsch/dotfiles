55fdb9df77a13d210b5f092fbe1db083925f6eb1
Conor Schaefer <conor.schaefer@gmail.com>
2012-06-01 11:19:14 -0400

    Improved formatting
    Added pipe call to remove_linebreaks.pl so that text gets processed into paragraphs and title is pretty.
    
    Currently call structure roughly passes query word to remove_linebreaks.pl for title formatting, and this will need to be adjusted when remove_linebreaks.pl is improved to make title formatting optional.

--- bin/etymology_lookup.pl
+++ bin/etymology_lookup.pl
@@ -11,11 +11,13 @@ my $word = $ARGV[0]; #get search term for use in URL request;
 #Call lynx, forget URLs, dump page text for search term;
 #Then get 20 lines of context after each occurrence of the search term;
 #Chop off the first 23 lines;
-#Chop off everything but the first 7 lines; 
+#Chop off everything but the first 15 lines; 
+#Feed text into remove_linebreaks for pretty formatting;
 my $page_dump = `lynx -nolist -dump "http://www.etymonline.com/index.php?search=$word" | \
                 grep -A 20 $word | \
                 tail -n +23 | \
-                head -n 7`; 
+                head -n 15 |
+                remove_linebreaks.pl $word`; 
 my $results = $page_dump; #Create new variable for final reporting actoins;
 $results =~ s/Look up \w+ at Dictionary\.com//g; #Remove alt-text for "Look up at Dictionary.com" links;
 chomp $results; #Remove any trailing whitespace at the end;
