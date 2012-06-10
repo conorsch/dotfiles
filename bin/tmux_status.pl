685fad69eb53c7a31e101c5dbd278c7f5dd9cb64
Conor Schaefer <conor.schaefer@gmail.com>
2012-06-10 15:56:02 -0400

    Added power_state function for grabbing and parsing acpi output.

--- bin/tmux_status.pl
+++ bin/tmux_status.pl
@@ -19,18 +19,42 @@ use Term::ANSIColor;
 #####LAPTOP#####
 my ($me, $power_state, $percent_charged, $time_left, $temp, $date, $time); #initialize variables to be constructed;
 
-sub me {
-    my $username = `whoami`;
-    my $hostname = `hostname`;
-    chomp ($username, $hostname);
+sub me { #return a 'me' identity in the form of user@hostname;
+    my $username = `whoami`; #grab username from shell;
+    my $hostname = `hostname`; #grab hostname from shell;
+    chomp ($username, $hostname); #remove trailing newlines;
     $me = "$username" . "@" . "$hostname"; #concatenate username and hostname with @ symbol;
-    return $me;
+    return $me; #pass concatenated 'me' back to function caller;
 }
 $me = me;
 
 sub power_state {
-    ... 
+    my $power_state; #initialize variable for construction;
+    my $charging_output =       "Battery 0: Charging, 62%, 00:49:05 until charged";
+    my $discharging_output =    "Battery 0: Discharging, 70%, 04:44:00 remaining";
+
+    my $acpi_battery = `acpi -b`; #grab battery output of acpi command for processing;
+    chomp $acpi_battery; #remove pesky trailing newline;
+
+    given ($acpi_battery) { #take a look at the output from acpi command;
+        when (/Discharging/) { #when AC adapter is unplugged;
+            $power_state = "drng"; } #set power_state to more succinct mention that battery is draining;
+        when (/Charging/) { #when AC adapter is plugged in;
+            $power_state = "chrg"; } #set power_state to more succinct mention that battery is charging;
+        when (/Charged/) { #when battery is fully charged;
+            $power_state = "full"; }  #set power-state to more succinct mention that battery if full;
+        default { say "power_state is currently unknown; please check script"; } #for unforeseen corner cases;
+    }
+
+    $acpi_battery =~ m/^(.*)(\d{2}%)(.*)$/; #look for 00% format percentage;
+    my $percent_charged = $2; #name second group (00% format, above) for returning;
+    $acpi_battery =~ m/^(.*)(\d{2}:\d{2}:\d{2})(.*)$/; #look for 00:00:00 format time;
+    my $time_left = $2; #name second group (00:00:00 format, above) for returning;
+
+    return ($power_state, $percent_charged, $time_left);  #pass power_state variable back to function caller;
 }
+($power_state, $percent_charged, $time_left) = power_state;
+            
 
 sub temp {
     my $acpi_temp = `acpi -t`; #grab temperature output from acpi command (in Celsius);
@@ -55,21 +79,16 @@ given ($acpi_output) {
     default { say "Must be charging... not set up yet!" }
 }
 
+sub build_status {
+    print "$me ";
+    print "$power_state ";
+    print "$percent_charged ";
+    print "$time_left ";
+    print color 'red';
 
-print $me;
-print " ";
-say "TEMP IS: $temp";
-print color 'red';
-print $acpi_output;
-print color 'reset';
+    print $temp; 
+    print color 'reset';
+    print "\n";
 
-
-print "\n";
-
-
-
-
-
-
-
-
+}
+build_status;
