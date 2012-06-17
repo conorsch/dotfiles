0b80413a6b7e02d78bd64290ade0843154bc9242
Conor Schaefer <conor.schaefer@gmail.com>
2012-06-17 14:36:43 -0400

    Better feedback: user's computer is now identified in the list.

--- bin/scan_local_ips.pl
+++ bin/scan_local_ips.pl
@@ -8,22 +8,28 @@ use diagnostics;
 use feature 'say'; #why use print?
 use List::Util qw/max/; #for nifty one-line hash walkthrough;
 
-my $network = `hostname -I`; #grab local IP address;
-chomp $network; #remove pesky trailing newline;
+my $internal_ip = `hostname -I`; #grab local IP address;
+chomp $internal_ip; #remove pesky trailing newline;
+$internal_ip =~ s/\s*//g; #remove any and all whitespace from network string;
+my $network = $internal_ip; #set aside original variable value, for use later;
 $network =~ s@(\d+\.\d+\.\d+)(\.\d+)@$1.1@g; #grab first three numbers, replace last with '1';
-chomp $network; #remove pesky trailing newline;
 $network =~ s/\s*//g; #remove any and all whitespace from network string;
-say "Scanning network $network for active hosts..."; #provide user feedback;
 my %host_list; #initialize hash to store IP addresses and hostnames;
 
+say "Scanning network $network for active hosts..."; #provide user feedback;
 my @nmap_output = `nmap -sP $network/24 | grep report`; #run nmap, capture output;
+
 foreach my $entry (@nmap_output) { #look at each captured line;
     chomp $entry; #remove pesky trailing newline;
-    $entry =~ s/Nmap scan report for //; #toss away cruft in output;
     next unless $entry =~ m/[\(|\)]/; #skip if report entry is malformed;
+
+    $entry =~ s/Nmap scan report for //; #toss away cruft in output;
     $entry =~ s/[\(|\)]//g; #remove parentheses around IP address;
     my ($host, $ip) = split(' ', $entry); #break up line into hostname and IP address;
-    $host = "unknown (probably router or gateway)" if $host =~ m/unknown/;
+
+    $host = "unknown (probably router or gateway)" if $host =~ m/unknown/; #usually correct;
+    $host .= " (This is you.)" if $ip =~ m/$internal_ip/; #identify current machine in list;
+
     $host_list{$ip} = $host; #store pair in hash, with ip as key;
 }
 my $str_length = max( map {length} keys %host_list ); #find longest string in hash keys;
