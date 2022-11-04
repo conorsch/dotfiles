beb86e803e1df3b7c366c8344926afd9d9ef77f9
Conor Schaefer <conor@ruin.dev>
2022-11-03 23:54:52 -0700

    fix: main branch

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,6 @@ sh -c "$(curl -fsLS get.chezmoi.io)"
 chezmoi_bin="${HOME}/bin/chezmoi"
 
 dotfiles_repo="https://git.ruin.dev/conor/dotfiles"
-dotfiles_branch="chezmoi"
+dotfiles_branch="main"
 
 "$chezmoi_bin" init --force "$dotfiles_repo" --branch "$dotfiles_branch" --apply
