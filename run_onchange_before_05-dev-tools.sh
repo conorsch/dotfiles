c2a5a0e96fcd007a1df063de5672d2ecb924eb96
Conor Schaefer <conor@ruin.dev>
2022-11-17 19:37:37 -0800

    install asdf

--- run_onchange_before_05-dev-tools.sh
+++ run_onchange_before_05-dev-tools.sh
@@ -13,3 +13,9 @@ rm -rf "$HOME/.gvm"
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 source ~/.gvm/scripts/gvm
 gvm install $(gvm listall | grep -P '^\s*go\d' | sort -V | tail -n 1 ) -B
+
+# get asdf
+if [[ ! -d "$HOME/.asdf" ]] ; then
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+    # asdf install | rg 'plugin is not installed' | perl -lanE 'print $F[0]' | xargs -r -n1 asdf plugin add
+fi
