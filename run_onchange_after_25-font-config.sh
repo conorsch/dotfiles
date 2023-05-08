34da965fc1925f311d762cb87aed10cba8dc00e7
Conor Schaefer <conor@ruin.dev>
2023-05-08 08:19:28 -0700

    fix: nerd font download uses stable url

--- run_onchange_after_25-font-config.sh
+++ run_onchange_after_25-font-config.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+# Download custom fonts for UTF-8 emoji display in terminals.
 set -eo pipefail
 
+
+# See recent releases at https://github.com/ryanoasis/nerd-fonts/releases
+nerd_fonts_release="v2.3.3"
 # font config (for starship emojis)
 font_dir="${HOME}/.local/share/fonts"
 font_path="${font_dir}/DejaVu Sans Mono Nerd Font Complete Mono.ttf"
-font_url="https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.ttf?raw=true"
+font_url="https://github.com/ryanoasis/nerd-fonts/blob/${nerd_fonts_release}/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.ttf?raw=true"
 if [[ ! -s "$font_path" ]]; then
     printf "Grabbing nerd fonts... "
     mkdir -p "$font_dir"
