8f92a6539e13e6fe2a28be479be5d49b7cfb3d80
Conor Schaefer <conor@ruin.dev>
2023-09-29 15:35:09 -0700

    fix: remove gvm
    Newer distros, both Debian and Fedora, error out.
    Looks like gvm isn't well maintained.
    
    Also switches compton -> picom, which resolves a Fedora 37 -> 38
    quibble.

--- run_onchange_before_05-dev-tools.sh
+++ run_onchange_before_05-dev-tools.sh
@@ -10,27 +10,13 @@ if [[ ! -e ~/.cargo ]] ; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 fi
 
-# https://github.com/moovweb/gvm
-if [[ ! -e ~/.gvm ]] ; then
-    echo "Getting gvm..."
-    bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-    echo "Sourcing gvm config..."
-    # shellcheck source=/dev/null
-    source ~/.gvm/scripts/gvm
-    latest_go="$(gvm listall | sed 's/ //g' | grep -P '^\s*go[\d.]+$' | sort -V | tail -n 1 )"
-    if [[ -z "$latest_go" ]]; then
-        >&2 echo "ERROR: Could not find latest go version"
-        exit 1
-    fi
-    gvm install "$latest_go" -B
-fi
-
 # get asdf
 if [[ ! -d "$HOME/.asdf" ]] ; then
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
     # asdf install | rg 'plugin is not installed' | perl -lanE 'print $F[0]' | xargs -r -n1 asdf plugin add
 fi
 
+echo "Setting up starship..."
 # install starship terminal prompt
 # https://starship.rs/guide/#%F0%9F%9A%80-installation
 source ~/.bashrc
