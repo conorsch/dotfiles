87847627b0a735b321476f8e46ab3da3c82a8a1e
Conor Schaefer <conor@ruin.dev>
2023-12-06 21:30:42 -0800

    feat: fedora39 support
    Started upgrading machines. This feels fairly comprehensive.
    Includes some improvements like writing cargo packages to
    /usr/local/bin/, which is long overdue.

--- justfile
+++ justfile
@@ -1,4 +1,5 @@
 lint:
+    # shellcheck lint
     fd -t f '(^executable_|bash|\.sh$)' -x file --mime-type \
         | perl -F': ' -lanE '$F[-1] =~ m#text/x-shellscript$# and say $F[0]' \
         | xargs -r shellcheck
--- run_onchange_before_05-dev-tools.sh
+++ run_onchange_before_05-dev-tools.sh
@@ -13,12 +13,15 @@ fi
 # get asdf
 if [[ ! -d "$HOME/.asdf" ]] ; then
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
-    # asdf install | rg 'plugin is not installed' | perl -lanE 'print $F[0]' | xargs -r -n1 asdf plugin add
 fi
+
+echo "Installing asdf plugins..."
+grep -P '^\w+' ~/.tool-versions | cut -d' ' -f1 | xargs -I{} asdf plugin add {}
 
 echo "Setting up starship..."
 # install starship terminal prompt
 # https://starship.rs/guide/#%F0%9F%9A%80-installation
+# shellcheck source=/dev/null
 source ~/.bashrc
 if ! hash starship > /dev/null 2>&1 ; then
     mkdir -p ~/bin
