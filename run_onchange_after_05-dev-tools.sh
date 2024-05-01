594c1030722b0c37da69e8b4089a8404b3ca2c3b
Conor Schaefer <conor@ruin.dev>
2024-05-01 16:51:09 -0700

    feat: more asdf -> devbox content

--- run_onchange_after_05-dev-tools.sh
+++ run_onchange_after_05-dev-tools.sh
@@ -16,22 +16,27 @@ rustup target add wasm32-unknown-unknown x86_64-unknown-linux-musl
 rustup update
 
 # get asdf
-if [[ ! -d "$HOME/.asdf" ]] ; then
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
-fi
+function _install_asdf() {
+    if [[ ! -d "$HOME/.asdf" ]] ; then
+        git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
+    fi
 
-# We need to source asdf directly, because bashrc/bash_profile
-# don't support interactive shells. Should they?
-echo "Installing asdf plugins..."
-# shellcheck source=/dev/null
-source ~/.asdf/asdf.sh
-if ! hash asdf > /dev/null 2>&1 ; then
-    >&2 echo "ERROR: asdf not found, but should be."
-    exit 1
-fi
-# Make sure necessary plugins are installed, with `|| true` because command
-# is not idempotent.
-grep -P '^\w+' ~/.tool-versions | cut -d' ' -f1 | xargs -I{} asdf plugin add {} || true
+    # We need to source asdf directly, because bashrc/bash_profile
+    # don't support interactive shells. Should they?
+    echo "Installing asdf plugins..."
+    # shellcheck source=/dev/null
+    source ~/.asdf/asdf.sh
+    if ! hash asdf > /dev/null 2>&1 ; then
+        >&2 echo "ERROR: asdf not found, but should be."
+        exit 1
+    fi
+    # Make sure necessary plugins are installed, with `|| true` because command
+    # is not idempotent.
+    grep -P '^\w+' ~/.tool-versions | cut -d' ' -f1 | xargs -I{} asdf plugin add {} || true
+}
+
+# Don't actually install: considering migrating from asdf -> devbox.
+# _install_asdf
 
 echo "Setting up starship..."
 # install starship terminal prompt
