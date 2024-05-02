4f564d6255c924f385caeb44f92ca3821e3425a1
Conor Schaefer <conor@ruin.dev>
2024-05-02 08:54:36 -0700

    feat: battery-monitor notifications in i3

---
+++ run_onchange_after_40-laptop-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Apply laptop-specific configuration for workstations.
+set -euo pipefail
+
+
+if [[ "$(hostnamectl status --json pretty | jq -r .Chassis)" != "laptop" ]]; then
+    >&2 echo "Not a laptop, skipping laptop configs..."
+    exit 0
+fi
+
+>&2 echo "Laptop detected, applying configs..."
+
+# Files have already been copied, so we can assume the battery-monitor
+# scripts and user-level systemd services are in place.
+systemctl --user daemon-reload
+systemctl --user enable --now battery-monitor
