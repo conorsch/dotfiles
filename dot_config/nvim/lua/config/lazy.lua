19840e672dd31f2c4326e87044e658b49e64dd30
Conor Schaefer <conor@ruin.dev>
2024-08-13 21:56:26 -0700

    feat: neovim rust config
    Coming together, slowly but surely.

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     -- import/override with your plugins
     { import = "plugins" },
   },
