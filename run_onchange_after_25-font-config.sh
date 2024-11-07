a3586e6ee2c0b7b4ff5c0400c51b61baf575477b
Conor Schaefer <conor@ruin.dev>
2024-11-07 12:11:56 -0800

    fix: use tar archives for font files
    Using `.zip` archives results in bigger downloads, but also is less
    less likely to work on fresh machines, due to `unzip` not being
    installed out of the box on most machines. Not sure how ubiquitous
    `xz` is but will follow-up with a dep update if required.

--- run_onchange_after_25-font-config.sh
+++ run_onchange_after_25-font-config.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 
 # See recent releases at https://github.com/ryanoasis/nerd-fonts/releases
 nerd_fonts_release="v3.2.1"
-nerd_font_archive_url="https://github.com/ryanoasis/nerd-fonts/releases/download/${nerd_fonts_release}/DejaVuSansMono.zip"
+nerd_font_archive_url="https://github.com/ryanoasis/nerd-fonts/releases/download/${nerd_fonts_release}/DejaVuSansMono.tar.xz"
 
 printf "Setting up nerd fonts... "
 # User-specific font dir.
@@ -17,7 +17,13 @@ nerd_font_archive_filepath="${font_dir}/$(basename "$nerd_font_archive_url")"
 # Download and clobber, since version could have changed.
 mkdir -p "$font_dir"
 curl -q -sSfL -o "$nerd_font_archive_filepath" "$nerd_font_archive_url"
-unzip -o -q -d "$font_dir" "$nerd_font_archive_filepath"
+# Use of `tar` will automatically understand `.tar.xz` archives,
+# as long as xz-utils is installed.
+tar -C "$font_dir" -xf "$nerd_font_archive_filepath"
+# Clean up the downloaded tarball post-extraction
+rm -f "$nerd_font_archive_filepath"
+rm -f "${font_dir:?}"/*.zip
+rm -f "${font_dir:?}"/*.tar.xz
 
 # Under what circumstances is it reasonable to assume `fc-cache` is present?
 if hash fc-cache > /dev/null 2>&1; then
