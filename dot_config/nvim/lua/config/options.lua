0c195191ee52bb4f28729ab0348de862c86cb6f6
Conor Schaefer <conor@ruin.dev>
2025-01-31 22:47:16 -0800

    i3: ditch borders
    Maximizing screen real estate by removing window title bars,
    and also trims the "gaps" which were a bit showy. Probably
    I've tipped too far the other way: right now with no window
    borders, it's hard to tell which window has focus. Still, pretty neat.
    
    Swapped the i3bar so it goes up top. Haven't figured out yet how to
    disable the bottom-line statusbar for neovim, would love to turn that
    off, too.

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -4,3 +4,9 @@
 
 -- Disable relative line numbers
 vim.wo.relativenumber = false
+
+-- Disable status bar at bottom
+-- Haven't figured out how to do this yet, none of these options work:
+-- vim.opt.laststatus = 0
+-- vim.o.laststatus = 0
+-- vim.laststatus = 0
