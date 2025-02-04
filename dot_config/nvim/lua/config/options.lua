ea9abf85f423ea4965831b9ffc389e989b6ea287
Conor Schaefer <conor@ruin.dev>
2025-02-04 12:51:23 -0800

    fix: nvim don't clobber clipboard

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -5,6 +5,11 @@
 -- Disable relative line numbers
 vim.wo.relativenumber = false
 
+-- Disable automatic clipboard clobbering, even when deleting via `dw`.
+vim.opt.clipboard = ""
+-- With `unnamedplus`, only yank operations go to clipboard, which could be useful.
+-- vim.opt.clipboard = "unnamedplus"
+
 -- Disable status bar at bottom
 -- Haven't figured out how to do this yet, none of these options work:
 -- vim.opt.laststatus = 0
