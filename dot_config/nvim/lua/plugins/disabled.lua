c110e0b5eb4b1783cb9d50d21ac59cc24cde0d70
Conor Schaefer <conor@ruin.dev>
2025-02-20 12:20:38 -0800

    fix(nvim): avoid hangs on exit
    Sometimes when very quickly opening a file, editing it, and
    saving-and-exiting again, the neovim instance hangs on an empty pane,
    blocking the terminal.

---
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+-- List of LazyVim plugins to opt-out of, by disabling them.
+return {
+  -- https://github.com/LazyVim/LazyVim/discussions/2190
+  { "akinsho/bufferline.nvim", enabled = false },
+  -- https://github.com/LazyVim/LazyVim/discussions/4356
+  { "folke/noice.nvim", enabled = false, commit = "d9328ef903168b6f52385a751eb384ae7e906c6f" },
+}
