a838484d6c9948d81c916475a487c548d90b82f1
Conor Schaefer <conor@ruin.dev>
2025-07-10 21:40:43 -0700

    fix: idempotent rust install

--- run_onchange_after_05-dev-tools.sh
+++ run_onchange_after_05-dev-tools.sh
@@ -2,18 +2,23 @@
 # Install common dev utils.
 set -eo pipefail
 
-# Bootstrap Rust environment, via https://rustup.rs/
-if [[ ! -e ~/.cargo ]] ; then
-    echo "Getting rust..."
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# Install rustup for cargo and rustc.
+if ! hash cargo > /dev/null 2>&1 ; then
+  # Bootstrap Rust environment, via https://rustup.rs/
+  if [[ ! -e ~/.cargo ]] ; then
+      echo "Getting rust..."
+      curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  fi
+
+  echo "Installing rust toolchains..."
+  # shellcheck source=/dev/null
+  source ~/.cargo/env
+  rustup component add rust-analyzer
+  rustup toolchain add nightly
+  rustup target add wasm32-unknown-unknown x86_64-unknown-linux-musl
+  rustup update
 fi
-echo "Installing rust toolchains..."
-# shellcheck source=/dev/null
-source ~/.cargo/env
-rustup component add rust-analyzer
-rustup toolchain add nightly
-rustup target add wasm32-unknown-unknown x86_64-unknown-linux-musl
-rustup update
 
 echo "Installing llm tooling"
 # opencode, via https://opencode.ai/docs/
