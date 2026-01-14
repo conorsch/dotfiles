bfce2b05e0cec7f4d2ea9f6e1d8570ae2ac672dc
Conor Schaefer <conor@ruin.dev>
2026-01-13 19:34:57 -0800

    chore: nix flake update

--- tools/gaming-vids/flake.nix
+++ tools/gaming-vids/flake.nix
@@ -2,7 +2,7 @@
   description = "Gaming clips organizer and reviewer";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
