dbcf5d8cceb11d352771235dd95f2e1f158a5d59
Conor Schaefer <conor@ruin.dev>
2026-02-11 12:46:33 -0800

    fix: slight adjustment to monitor setup

--- dot_screenlayout/private_executable_pantograph-vertical-petrichor.sh
+++ dot_screenlayout/private_executable_pantograph-vertical-petrichor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP --primary --mode 2880x1920 --pos 1440x1864 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --mode 2560x1440 --pos 0x0 --rotate right --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off
+xrandr --output eDP --primary --mode 2880x1920 --pos 1440x1832 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --mode 2560x1440 --pos 0x0 --rotate right --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off
