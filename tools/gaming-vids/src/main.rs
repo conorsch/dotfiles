7921dcda8bc485f74e10322deb21b01a50806276
Conor Schaefer <conor@ruin.dev>
2026-03-12 10:34:15 -0700

    fix: gaming-vids logs to stderr

--- tools/gaming-vids/src/main.rs
+++ tools/gaming-vids/src/main.rs
@@ -135,6 +135,7 @@ enum Commands {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt()
+        .with_writer(std::io::stderr)
         .with_env_filter(
             tracing_subscriber::EnvFilter::try_from_default_env()
                 .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
