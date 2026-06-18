35efe3e0c47a5caea84557acc61ffde708de7f74
Conor Schaefer <conor@ruin.dev>
2026-06-18 09:23:22 -0700

    chore: nixpkgs 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Dev shell for managing ruin.dev dotfiles";
   # name = "ruindev-dotfiles";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
