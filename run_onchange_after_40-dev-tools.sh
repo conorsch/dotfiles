8f3fe651cdb314968dcd45a22536a72c665afaaa
Conor Schaefer <conor@ruin.dev>
2026-06-18 09:54:06 -0700

    fix: claude-code installation
    The pnpm v11 emits "no packages found" to stdout which is crazy.
    Anyway, we can't filter based on that anymore.
    
    fix: pnpm config problem

--- run_onchange_after_40-dev-tools.sh
+++ run_onchange_after_40-dev-tools.sh
@@ -3,7 +3,10 @@ set -euo pipefail
 
 # shellcheck source=dot_bashrc
 source "${HOME:?}/.bashrc"
+
 # vim config
 gum spin --title "Syncing vim plugins... " \
   vim +PlugInstall +qall
 
+# pnpm config (required v10 -> v11)
+pnpm config set global-bin-dir "${HOME:?}/.local/share/pnpm"
